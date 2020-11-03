d998fcafb0db85406a26a7ff75a75255bf027a64
Igor Tishenko <tishenko.i.y@gmail.com>
2020-11-03 12:09:04 +0500

    create message dto

---
+++ SP.EventBus/Events/IUserChangeMessage.cs
@@ -0,0 +1,14 @@
+﻿using SP.EventBus.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP.EventBus.Events
+{
+    interface IUserChangeMessage
+    {
+        Guid Id { get; set; }
+        UserDTO User { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
---
+++ SP.EventBus/Models/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP.EventBus.Models
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string MiddleName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
